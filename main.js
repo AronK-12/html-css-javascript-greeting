71bdce5c1f2b36692b66bd4f9bab2a51bae5a633
aronk-112 <aronkvaszta12@outlook.com>
2024-01-14 16:18:54 +0100

    Added age, and conditional greetings

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 const submitButton = document.getElementById('submit-btn');
-const greetingText = document.getElementById('greeting-txt');
+
+const nameInput = document.getElementById('greeting-txt');
+const ageInput = document.getElementById('age-text');
 
 let userName;
+let userAge;
 
 submitButton.onclick = () => {
-  userName = document.getElementById('text-box').value;
+  userName = document.getElementById('name-box').value;
+  userAge = document.getElementById('age-box').value;
 
-  if (userName === '') {
-    greetingText.innerText = "I can't greet nobody.";
+  if (!userName && !userAge) {
+    nameInput.innerText = 'I know nothing about you.';
+    ageInput.innerText = '';
+  } else if (userName && !userAge) {
+    nameInput.innerText = `Hello, ${userName}.`;
+    ageInput.innerText = '';
+  } else if (!userName && userAge) {
+    nameInput.innerText = "I don't know you.";
+    ageInput.innerText = `But you're ${userAge} years old.`;
   } else {
-    greetingText.innerText = `Hello, ${userName}!`;
+    nameInput.innerText = `Hello, ${userName}!`;
+    ageInput.innerText = `You're ${userAge} years old.`;
   }
 };
